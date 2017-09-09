d67a1c7d76b6e3e3c7e28179e733e636c4387194
coder-god <407410014@qq.com>
2017-09-09 23:03:29 +0800

    add SortFactory.java

---
+++ src/com/interw/algorithms/sort/SortFactory.java
@@ -0,0 +1,18 @@
+package com.interw.algorithms.sort;
+
+/**
+ * 动态生成排序实例的工厂
+ */
+public class SortFactory {
+	public static Sorts getClass(Class<?> clazz) {
+		
+		Sorts sort = null;
+		
+		try {
+			sort = (Sorts) Class.forName(clazz.getName()).newInstance();
+		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return sort;
+	}
+}
